3b6ae82a8a235df34d22b140c4707e3a3ef38bc5
git <kallin.robert@gmail.com>
2019-06-17 20:08:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pybeerbolaget
+﻿# pybeerbolaget
+
+_A python package to get information about the latest beer available at Systembolaget in Sweden._
+
+Documentation:
+
+https://github.com/Ceerbeerus/pybeerbolaget
---
+++ setup.py
@@ -0,0 +1,20 @@
+﻿"""Setup configuration."""
+import setuptools
+
+setuptools.setup(
+    name="pybeerbolaget",
+    version='0.0.1',
+    author="Robert Kallin",
+    author_email="kallin.roberts@gmail.com",
+    description="A python package to get information about the latest beer available at Systembolaget.",
+    long_description="A python package to get information about the latest beer available at Systembolaget.",
+    install_requires=['requests'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ceerbeerus/pybeerbolaget",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
