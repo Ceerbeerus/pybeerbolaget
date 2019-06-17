abe84949c76e7189e5764f8cfddc7f3e96061a3e
git <kallin.robert@gmail.com>
2019-06-18 00:35:51 +0200

    Added api call.

---
+++ pybeerbolaget/ha_custom/beer.py
@@ -0,0 +1,35 @@
+﻿from pybeerbolaget.ha_custom import common
+
+class beer_handler():
+    def __init__(self, api_key, show_beer):
+        self.api_key = api_key
+        self.show_beer = show_beer
+        self.beers = []
+        self.release = None
+
+    async def update_new_beers(self):
+        if not self.release:
+            beer_available = await common.get_beverage(self.api_key, 'Öl')
+        self.release = beer_available[-1]['SellStartDate']
+        for beer_item in beer_available:
+            if beer_item['SellStartDate'] == self.release:
+                new_beer = beer('beer', beer_item['ProducerName'], beer_item['ProductNameBold'], beer_item['ProductNameThin'],
+                                beer_item['Type'], beer_item['Price'], beer_item['Country'])
+                self.beers.append(new_beer)
+    
+    async def get_beers(self):
+        return json.dumps(self.beers.__dict__)
+
+    async def get_release(self):
+        return self.release
+
+class beer():
+    def __init__(self, beverage_type, brewery, name, detailed_name, type, price, country):
+        self.beverage_type = beverage_type
+        self.brewery = brewery
+        self.name = name
+        self.detailed_name = detailed_name
+        self.type = type
+        self.price = price
+        self.country = country
+        self.rating = None
---
+++ pybeerbolaget/ha_custom/common.py
@@ -0,0 +1,30 @@
+﻿from datetime import date, timedelta
+import urllib.parse
+import json
+import requests
+
+async def get_beverage(api_key, type):
+    SellStartDateFrom = date.today() - timedelta(days=31)
+    SellStartDateTo = date.today() + timedelta(days=31)
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': api_key,
+        'content-type': 'application/json'
+    }
+
+    url = 'api-extern.systembolaget.se/product/v1/product/search?'
+    params = urllib.parse.urlencode({
+        # Request parameters
+        'SubCategory': type,
+        'AssortmentText': 'Små partier',
+        'SellStartDateFrom': SellStartDateFrom,
+        'SellStartDateTo': SellStartDateTo,
+    })
+    all_beverages = []
+    try:
+        all_beverages = requests.get('https://api-extern.systembolaget.se/product/v1/product/search?%s' % params, headers=headers, verify=True).json()['Hits']
+        all_beverages = sorted(all_beverages, key=lambda k: k['SellStartDate'])
+    except:
+        print ("Could not fetch data from api.")
+
+    return all_beverages
