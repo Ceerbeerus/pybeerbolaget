c3a96c9cbff774622e3a3b2ee16ac6542c027382
Ceerbeerus <46457478+Ceerbeerus@users.noreply.github.com>
2019-11-27 00:16:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="beerbolaget",
-    version='0.2.14',
+    version='0.2.15',
     author="Robert Kallin",
     author_email="kallin.roberts@gmail.com",
     description="A python package to get information about the latest beer available at Systembolaget.",
